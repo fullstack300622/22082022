add4056c2b5aa2522caa84f42ae49e2314e9ac9b
fullstack300622 <108460728+fullstack300622@users.noreply.github.com>
2022-08-22 19:25:27 +0300

    Create forEach.java

---
+++ forEach.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        
+//       ArrayList<String> myArrayList = new ArrayList<>();
+//        myArrayList.add("One");
+//        myArrayList.add("Two");
+//        myArrayList.add("Three");
+
+//        for (int i = 0; i < myArrayList.size(); i++) {
+//            System.out.println(myArrayList.get(i));
+//        }
+
+
+//        for (String s : myArrayList) {
+//        }
+
+
+//        int[] numbersArray = {3, 7, 5, 16, 8};
+//
+//        for (int number : numbersArray) {
+//            System.out.println(number);
+//        }
+//
+//        int[][] numbersArray = {{3, 7, 5}, {16, 8}};
+//        for (int innerArray[] : numbersArray) {
+//            for (int number :innerArray) {
+//                System.out.println(number);
+//            }
+//        }
+    }
+
+}
