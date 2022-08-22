8b6f46eac1ad48cd1361df1bf97056812d6f2186
fullstack300622 <108460728+fullstack300622@users.noreply.github.com>
2022-08-22 19:29:14 +0300

    Create scopeTwo.java

---
+++ scopeTwo.java
@@ -0,0 +1,17 @@
+public class Main {
+    static int c=5;
+
+    public static void main(String[] args) {
+        int i=5;
+
+        for (i = 0; i < 5; i++) {
+            int x;
+        }
+        System.out.println(c);
+    }
+
+
+    public static void printNumber(){
+//        System.out.println(i);
+    }
+}
